53d5a5f145aef7dd4ff919b51dc01f4a97df2915
Sergey J <aleks.s.educ@gmail.com>
2023-02-05 18:03:36 +0200

    Improve component Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { addContact } from '../../redux/operations';
 import { addContact } from '../../redux/contacts/operations';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './Form.module.css';
 import { selectContactsData } from '../../redux/contacts/selectors';
@@ -21,36 +21,26 @@ export function Form() {
   const contactsFromStore = useSelector(selectContactsData);
   const dispatch = useDispatch();
 
-  const handleFormData = (newName, newNumber) => {
+  const handleFormSubmit = event => {
+    event.preventDefault();
     const arrayOfNames = contactsFromStore.map(contact => contact.name);
-    const nameToLowerCase = newName.toLowerCase();
+    const nameToLowerCase = name.toLowerCase();
     const nameIsPresent = arrayOfNames.find(
       element => element.toLowerCase() === nameToLowerCase
     );
-    if (!nameIsPresent) {
-      const newContact = {
-        // id: nanoid(),
-        name: newName,
-        number: newNumber,
-      };
-      dispatch(addContact(newContact));
-      return true;
+    if (nameIsPresent) {
+      return toast.info(`${name} is already in contacts`, {
+        position: 'top-center',
+        autoClose: 3000,
+        theme: 'colored',
+      });
     }
-    // return alert(`${newName} is already in contacts`);
-
-    toast.info(`${newName} is already in contacts`, {
-      position: 'top-center',
-      autoClose: 3000,
-      theme: 'colored',
-    });
-  };
-
-  const handleFormSubmit = event => {
-    event.preventDefault();
-    const onSubmitResult = handleFormData(name, number);
-    if (onSubmitResult) {
-      formCleaner();
-    }
+    const newContact = {
+      name: name,
+      number: number,
+    };
+    dispatch(addContact(newContact));
+    formCleaner();
   };
 
   const formCleaner = () => {
@@ -59,39 +49,36 @@ export function Form() {
   };
 
   return (
-    <>
-      <form className={css.form} onSubmit={handleFormSubmit}>
-        <label className={css.label}>
-          Name
-          <input
-            className={css.nameInput}
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleNameChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            className={css.nameInput}
-            type="tel"
-            name="number"
-            value={number}
-            onChange={handleNumberChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button className={css.addBatton} type="submit">
-          Add contact
-        </button>
-      </form>
-      <ToastContainer position="top-center" autoClose={3000} />
-    </>
+    <form className={css.form} onSubmit={handleFormSubmit}>
+      <label className={css.label}>
+        Name
+        <input
+          className={css.nameInput}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          className={css.nameInput}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleNumberChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button className={css.addBatton} type="submit">
+        Add contact
+      </button>
+    </form>
   );
 }
